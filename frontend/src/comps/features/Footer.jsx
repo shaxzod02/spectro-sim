cec86a3924849f79c01071a8e439a45ca5f18393
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-06-21 00:24:50 +0800

    Update Footer.jsx

--- frontend/src/comps/features/Footer.jsx
+++ frontend/src/comps/features/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
         text-slate-300 
         text-center 
         py-6 border-t">
-            <p>© 2025 <Link className="hover:underline lg:hover:text-cyan-300" to="/" target="_blank">SpectroSim</Link> • Powered by <Link className="hover:underline lg:hover:text-cyan-300" to="https://pubchem.ncbi.nlm.nih.gov" target="_blank">PubChem</Link> • <Link className="hover:underline hover:text-cyan-300" to="https://github.com/michaelddeming/spectro-sim" target="_blank">GitHub</Link></p>
+            <p>© 2025 <Link className="hover:underline lg:hover:text-cyan-300" to="/" target="_blank">SpectroSim</Link> • Powered by <Link className="hover:underline lg:hover:text-cyan-300" to="https://pubchem.ncbi.nlm.nih.gov" target="_blank">PubChem</Link> • <Link className="hover:underline hover:text-cyan-300" to="https://github.com/shaxzod02/spectro-sim" target="_blank">GitHub</Link></p>
             
                 
      
